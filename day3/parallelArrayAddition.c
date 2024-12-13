2ab13e5795e45bd4a2d58356449b191c82d32bc5
Cisco Ramon <cisssco581b@gmail.com>
2024-12-13 16:54:53 +0530

    Updated on Dec-13 at 16:54

---
+++ day3/parallelArrayAddition.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+#define N 100000
+#define T 10
+
+int* a, *b, *c;
+
+void* task(void* threadId){
+    int tid = *(int*)threadId;
+    int chunksize = N / T;
+    int start = tid * chunksize;
+    int end = start + chunksize;
+    if(tid == T - 1) end = N;
+
+    for(int i = start; i < end; i++){
+        c[i] = a[i] + b[i];
+    }
+}
+
+int main(){
+    a = (int*) malloc(sizeof(int) * N);
+    b = (int*) malloc(sizeof(int) * N);
+    c = (int*) malloc(sizeof(int) * N);
+    for(int i = 0; i < N; i++){
+        a[i] = i + 1;
+        b[i] = i + 1;
+        c[i] = 0;
+    }
+
+    pthread_t t[T];
+    for(int i = 0; i < T; i++){
+        int* a = (int*) malloc(sizeof(int));
+        *a = i;
+        pthread_create(&t[i], NULL, task, (void*)a);
+    }
+
+    for(int i = 0; i < T; i++){
+        pthread_join(t[i], NULL);
+    }
+
+    for(int i = 0; i < N; i++){
+        printf("%d ", c[i]);
+    }
+    return 0;
+}
