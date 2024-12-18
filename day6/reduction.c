715d9ac59f3c72e800994b4b44226e8f95dca66e
Cisco Ramon <cisssco581b@gmail.com>
2024-12-18 11:58:46 +0530

    Updated on Dec-18 at 11:58

---
+++ day6/reduction.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<omp.h>
+#define N 1000000000
+#define T 13
+int main(){
+    long long sum = 0;
+    #pragma omp parallel num_threads(T)
+    {
+        #pragma omp for reduction(+ : sum)
+        for(int i = 0; i < N; i++){
+            sum += i + 1;
+        }
+    }
+
+
+    printf("Calculated sum = %lld\n", sum);
+    long long expectedSum = (N * ((N + 1) * 1L) / 2);
+    printf("Expected sum = %lld\n", expectedSum);
+    if(sum == expectedSum){
+        printf("_____Passed_____\n");
+    }
+    else printf("_____Failed_____\n");
+
+    return 0;
+}
