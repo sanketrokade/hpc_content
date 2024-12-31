e06a0f3acdf5b46a9f364eaf6e58a94c022f0f5d
Cisco Ramon <cisssco581b@gmail.com>
2024-12-31 16:52:37 +0530

    Updated on Dec-31 at 16:52

--- day12/tag.c
+++ day12/tag.c
@@ -6,17 +6,32 @@ int main() {
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    int data1, data2;
+    int data1, data2, data3, data4, data5;
     if(rank == 0){
         data1 = 100;
         data2 = 200;
-        MPI_Send(&data1, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        MPI_Send(&data2, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        data3 = 300;
+        data4 = 400;
+        data5 = 500;
+        MPI_Request request;
+        MPI_Isend(&data1, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request);
+        MPI_Isend(&data2, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request);
+        MPI_Isend(&data3, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request);
+        MPI_Isend(&data4, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request);
+        MPI_Isend(&data5, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request);
     }
     else if(rank == 1){
         MPI_Recv(&data1, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Recv(&data2, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("process %d received data1 %d data2 %d\n", rank, data1, data2);
+        MPI_Recv(&data3, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&data4, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&data5, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("process %d\n", rank);
+        printf("data1 %d\n", data1);
+        printf("data2 %d\n", data2);
+        printf("data3 %d\n", data3);
+        printf("data4 %d\n", data4);
+        printf("data5 %d\n", data5);
     }
 
     MPI_Finalize();
