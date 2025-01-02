d4f19a8712cab58979ceb888e3f8cabac1346617
Cisco Ramon <cisssco581b@gmail.com>
2025-01-02 16:11:16 +0530

    Updated on Jan-02 at 16:11

--- day14/arrSum1.c
+++ day14/arrSum1.c
@@ -12,21 +12,21 @@ int main() {
     const long long N = 1000000000;
     double startTime, endTime;
     long long chunksize = N / size;
-    long long* global_arr;
+    long long* global_arr = NULL;
     long long* local_arr = (long long*)malloc(chunksize * sizeof(long long));
 
     if (rank == 0) {
         global_arr = (long long*)malloc(N * sizeof(long long));
-        for (long long i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             global_arr[i] = i + 1;
         }
     }
 
     if(rank == 0) startTime = MPI_Wtime();
-    MPI_Scatter(global_arr, chunksize, MPI_LONG_LONG, local_arr, chunksize, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
+    MPI_Scatter(global_arr, chunksize, MPI_LONG_LONG_INT, local_arr, chunksize, MPI_LONG_LONG_INT, 0, MPI_COMM_WORLD);
 
     long long local_sum = 0;
-    for (long long i = 0; i < chunksize; i++) {
+    for (int i = 0; i < chunksize; i++) {
         local_sum += local_arr[i];
     }
 
@@ -35,7 +35,7 @@ int main() {
         global_sums = (long long*)malloc(size * sizeof(long long));
     }
 
-    MPI_Gather(&local_sum, 1, MPI_LONG_LONG, global_sums, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
+    MPI_Gather(&local_sum, 1, MPI_LONG_LONG_INT, global_sums, 1, MPI_LONG_LONG_INT, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
         long long total_sum = 0;
